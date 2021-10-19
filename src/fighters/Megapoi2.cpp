a874fbcb23afb9fef5ea43bf62039681de26925a
MEUNIER Adrien <adrien.meunier42@gmail.com>
2021-10-19 15:55:22 +0200

    Le bot est fini et est parfait

--- src/fighters/Megapoi2.cpp
+++ src/fighters/Megapoi2.cpp
@@ -6,14 +6,15 @@
 #include "Megapoi2.h"
 
 using namespace std;
-//Ceci est un bot invinsible, comme ca celui-ci ne peut pas perdre
+//Ceci est un bot invinsible, comme ca celui-ci ne peut pas perdre 
+//A la fin des rounds il sera déclaré gagnant car il a tout ses points de vie
 Megapoi2::Megapoi2() : FighterBot("Megapoi2", 0, 0, 30) {
     this->targetId = "";
 }
 
 
 bool Megapoi2::VerifFighter(Arena arena, int i, int j) { 
-    ActionMove* action = new ActionMove(this->getX()+i,this->getY()+j); //Crée une action comme si le bot allait se déplacer dans la case séléctionné
+    ActionMove* action = new ActionMove(i,j); //Crée une action comme si le bot allait se déplacer dans la case séléctionné
     action->setArena(&arena); 
     action->setFighter(this); 
     std::vector<Fighter> list = arena.get(this->getX()+i,this->getY()+j); //Prend la liste des fighter qui sont sur la case
@@ -38,10 +39,10 @@ Action* Megapoi2::choose(Arena arena) {
     if(DangerSurCase == true){
         for (int i = -1; i < 2; i++) {
             for (int j  = -1; j < 2; j++) {   
-          
-          
+                
+                
                 if(VerifFighter(arena,i,j)){ //Lance la fonction VerifFighter, et fait le if si il return "true"
-                    action = new ActionMove(this->getX()+i,this->getY()+j); //Deplace le bot sur une case non utilisé
+                    action = new ActionMove(i,j); //Deplace le bot sur une case non utilisé
                 }
             }
         }              
